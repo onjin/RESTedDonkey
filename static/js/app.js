bcc8f99348f1c02087073b7f635e16d993a510f0
Marek Wywiał <onjinx@gmail.com>
2013-07-13 00:54:33 +0200

    Formated app.js

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,13 @@
 angular.module('donkey', ['resourceFilters', 'resourceServices']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/resources', {templateUrl: '/_static/partials/resource-list.html',   controller: ResourceListCtrl}).
-      when('/resources/:resourceId', {templateUrl: '/_static/partials/resource-detail.html', controller: ResourceDetailCtrl}).
+      when('/resources', {
+          templateUrl: '/_static/partials/resource-list.html',
+          controller: ResourceListCtrl
+      }).
+      when('/resources/:resourceId', {
+          templateUrl: '/_static/partials/resource-detail.html',
+          controller: ResourceDetailCtrl
+      }).
       otherwise({redirectTo: '/resources'});
 }]);
